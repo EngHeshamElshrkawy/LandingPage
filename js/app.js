981d393fa078a520820d143a05d202647b1182d3
Hesham Elshrkawy <Hesham.45645@gmail.com>
2021-09-12 17:08:11 +0200

    1-Added a dynamically generated navigation bar 2-Added navigation functionality 3-Now navigation bar shows which section is clicked by changing background color

--- js/app.js
+++ js/app.js
@@ -17,15 +17,27 @@
  * Define Global Variables
  * 
 */
-
+let sectionsList = document.querySelectorAll("section");
+let navigationList = document.getElementById("navbar__list");
+let ul = [];
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function populateNavigationBar(section){
+    const listItem = document.createElement('li');
+	listItem.textContent = section.getAttribute("data-nav");
+    ul.push(listItem);
+	navigationList.appendChild(listItem);
+}
 
-
+function resetUl(){
+    ul.forEach(function(element){
+        element.style.background = "transparent";
+    });
+}
 
 /**
  * End Helper Functions
@@ -34,12 +46,21 @@
 */
 
 // build the nav
-
+sectionsList.forEach(populateNavigationBar);
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
+navigationList.addEventListener('click', function(e){
+    if(e.target != navigationList){
+        resetUl();
+        e.target.style.background = "rgba(136,203,171,0.5)";
+    }
+    sectionsList[ul.indexOf(e.target)].scrollIntoView();
+    
+});
+
 
 
 /**
@@ -53,5 +74,4 @@
 // Scroll to section on link click
 
 // Set sections as active
-
 
