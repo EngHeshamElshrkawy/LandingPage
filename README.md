1334198f78e62d8acd7d05fe881e67282f1f5c1f
Hesham Elshrkawy <Hesham.45645@gmail.com>
2021-09-17 14:05:14 +0200

    Modified the readme.md

--- README.md
+++ README.md
@@ -1,13 +1,42 @@
 # Landing Page Project
 
-## Table of Contents
+# Demo-Preview
 
-* [Instructions](#instructions)
+![Preview of the page](/sources/preview_1.png)
 
-## Instructions
+![Scrolling through the page](/sources/preview_2.png)
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+# Table of Contents
 
-To get started, open `js/app.js` and start building out the app's functionality
+* [Project Title](#landing-page-project)
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+* [Demo-Preview](#demo-preview)
+
+* [Table of Contents](#table-of-contents)
+
+* [Project Description](#project-description)
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+# Project Description
+
+This project is a landing page for a website.
+It contains sections that can be scrolled to using a dynamically built navigation bar that is hidden whenever the user stops scrolling for a few seconds.
+
+The project is written in HTML and CSS, with the use of JavaScript to manipulate the DOM.
+
+[(Back to top)](#table-of-contents)
+# Installation
+
+To run this project on your machine all you have to do is download this repo and extract it into your desired location.
+
+[(Back to top)](#table-of-contents)
+# Usage
+
+Simply double click the index.html file, or drag it into your prefered browser. 
+You can test the functionality by clicking any item from the navigation bar, or scrolling down to a certain section. There's also a scroll to top button that shows after scrolling a screen fold.
+
+[(Back to top)](#table-of-contents)
+
--- js/app.js
+++ js/app.js
@@ -30,6 +30,10 @@ let mainHero = document.getElementsByClassName("main__hero")[0];
  * Start Helper Functions
  * 
 */
+
+//This function takes a section as parameter and creates a list item that corresponds to this section
+//by setting it's content to the section's name, it then adds this list item to both an array that contains all the list items,
+//and to the unordered list in the header.
 function populateNavigationBar(section){
     const listItem = document.createElement('li');
 	listItem.textContent = section.getAttribute("data-nav");
@@ -37,6 +41,9 @@ function populateNavigationBar(section){
 	navigationList.appendChild(listItem);
 }
 
+
+//This function iterates through all the list items in the unordered list of the navigation bar and sets its background to transparent.
+//The use of this function is to remove the highlighting of sections that are no longer in viewport.
 function resetUl(){
     ul.forEach(function(element){
         element.style.background = "transparent";
@@ -44,15 +51,16 @@ function resetUl(){
     });
 }
 
-
+//This function takes a section as a parameter and returns true if the section is in viewport, false otherwise.
 function sectionInViewport(section) {
     const boundries = section.getBoundingClientRect();
     return (
-        boundries.top <= (window.innerHeight /4 || document.documentElement.clientHeight/4) &&
-        boundries.bottom >= (window.innerHeight /4 || document.documentElement.clientHeight/4)
+        boundries.top <= (window.innerHeight || document.documentElement.clientHeight)/4 &&
+        boundries.bottom >= (window.innerHeight || document.documentElement.clientHeight)/4
     );
 }
 
+//This function returns the view back to the top of the page.
 function scrollToTop(){
     document.documentElement.scrollTop = 0;
 }
@@ -64,9 +72,14 @@ function scrollToTop(){
 */
 
 // build the nav
+//For each section in the sections list adds that section to the navigation bar.
 sectionsList.forEach(populateNavigationBar);
 
 // Add class 'active' to section when near top of viewport
+//On scrolling this block of code checks for every section in the sections list if it exists within the viewport.
+//It then adds or remove the class "your-active-class" to this section.
+//After that it matches the section that's within the viewport with its corresponding navigation list item to highlight said item,
+//or to remove the highlight from items that correspond to sections that are no longer in viewport.
 document.addEventListener('scroll', function (e) {
     sectionsList.forEach(function(section){
         if(sectionInViewport(section)){
@@ -95,6 +108,8 @@ document.addEventListener('scroll', function (e) {
 });
 
 // Scroll to anchor ID using scrollTO event
+//On click event on the navigation bar, this block of code scrolls the view to the target section, removes previous highlighting
+//of navigation list, and highlights the new list item.
 navigationList.addEventListener('click', function(e){
     if(e.target != navigationList){
         resetUl();
@@ -104,7 +119,7 @@ navigationList.addEventListener('click', function(e){
     
 });
 
-//Changing the visibility of the scroll to top button
+//Changing the visibility of the scroll to top button based on whether the user scrolled down past the page's fold or not.
 document.addEventListener("scroll", function(){
     const rect = mainHero.getBoundingClientRect();
     if(rect.top <= -413){
@@ -118,11 +133,12 @@ document.addEventListener("scroll", function(){
 topButton.addEventListener('click', scrollToTop); 
 
 
-//Hide navigation bar
+//Hide navigation bar if the user didn't scroll for 3 seconds.
 hidingNavigationBar = setTimeout(function(){
     navigationBar.style.visibility = "hidden";
 }, 3000);
 
+//If the user scrolls, the navigation bar is set to visible again and the timer resets.
 document.addEventListener("scroll", function(){
     navigationBar.style.visibility = "visible";
     clearTimeout(hidingNavigationBar);
@@ -131,7 +147,7 @@ document.addEventListener("scroll", function(){
     }, 3000);
 });
 
-
+//If the user hovers over the navigation bar, it resets the timer for hiding the navigation bar and sets it to visible.
 document.addEventListener("mousemove", function(e){
     if(e.clientX <= 755 && e.clientX >=0 && e.clientY >=0 && e.clientY <= 22){
         navigationBar.style.visibility = "visible";
@@ -144,14 +160,7 @@ document.addEventListener("mousemove", function(e){
 
 /**
  * End Main Functions
- * Begin Events
- * 
 */
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
